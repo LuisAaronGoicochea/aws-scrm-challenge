f788055c451cd1247ec2e903970d8ad00b617372
Luis Aaron Goicochea Alfaro <luis.goicochea@sider.com.pe>
2023-06-13 23:01:06 -0500

    prueba

--- src/data_processor.py
+++ src/data_processor.py
@@ -28,157 +28,49 @@ class DataProcessor:
             options = merged_options
 
         return self.spark.read.format(format).options(**options).load(path)
+    
+    def update_dataframe(self, base_df, new_df, select_columns, join_columns):
+        new_df = new_df.select(*select_columns)
+        return base_df.union(new_df)
+    
+    def distinct_stores(self, base_df, join_columns, group_by_columns, distinct_count_column, result_column):
+        distinct_stores_df = base_df \
+            .join(base_df, join_columns) \
+            .groupBy(group_by_columns) \
+            .agg(countDistinct(distinct_count_column).alias(result_column))
+        return distinct_stores_df
+    
+    def calculate_second_most_selling(self, base_df, arguments):
+        join_columns = arguments.get('join_columns')
+        group_by_columns = arguments.get('group_by_columns')
+        quantity_column = arguments.get('quantity_column')
+        rank_column = arguments.get('rank_column')
+        select_columns = arguments.get('select_columns')
 
-    def calculate_total_quantity(self, data_df, group_by_column, quantity_column):
-        """
-        Calcula la cantidad total agrupada por una columna en un DataFrame.
+        window_spec = Window.partitionBy(group_by_columns).orderBy(desc(quantity_column))
 
-        Args:
-            data_df (pyspark.sql.DataFrame): DataFrame de entrada.
-            group_by_column (str): Nombre de la columna por la cual agrupar.
-            quantity_column (str): Nombre de la columna de cantidad.
-
-        Retorna:
-            pyspark.sql.DataFrame: DataFrame con la cantidad total por grupo.
-        """
-        return data_df.groupBy(group_by_column).agg(sum(quantity_column).alias("total_quantity"))
-
-    def join_tables(self, left_df, right_df, join_columns, select_columns):
-        """
-        Realiza una operación de join entre dos DataFrames.
-
-        Args:
-            left_df (pyspark.sql.DataFrame): DataFrame izquierdo.
-            right_df (pyspark.sql.DataFrame): DataFrame derecho.
-            join_columns (list): Lista de columnas para realizar el join.
-            select_columns (list): Lista de columnas para seleccionar en el DataFrame resultante.
-
-        Retorna:
-            pyspark.sql.DataFrame: DataFrame resultante después del join y selección de columnas.
-        """
-        return left_df.join(right_df, join_columns).select(select_columns)
-
-    def count_distinct_values(self, data_df, group_by_columns, count_column, alias_name):
-        """
-        Calcula el número de valores distintos en una columna en un DataFrame.
-
-        Args:
-            data_df (pyspark.sql.DataFrame): DataFrame de entrada.
-            group_by_columns (list): Lista de columnas por las cuales agrupar.
-            count_column (str): Nombre de la columna para contar valores distintos.
-            alias_name (str): Nombre del alias para el resultado.
-
-        Retorna:
-            pyspark.sql.DataFrame: DataFrame con el conteo de valores distintos por grupo.
-        """
-        return data_df.groupBy(group_by_columns).agg(countDistinct(count_column).alias(alias_name))
-
-    def calculate_ranked_values(self, data_df, group_by_columns, value_column, rank_column):
-        """
-        Calcula los valores clasificados por grupos en un DataFrame.
-
-        Args:
-            data_df (pyspark.sql.DataFrame): DataFrame de entrada.
-            group_by_columns (list): Lista de columnas por las cuales agrupar.
-            value_column (str): Nombre de la columna de valores.
-            rank_column (str): Nombre de la columna para el rango.
-
-        Retorna:
-            pyspark.sql.DataFrame: DataFrame con los valores clasificados por grupos y rango.
-        """
-        window_spec = Window.partitionBy(group_by_columns).orderBy(desc(value_column))
-        return data_df.groupBy(group_by_columns).agg(sum(value_column).alias(value_column)) \
+        ranked_df = base_df \
+            .groupBy(*join_columns) \
+            .agg(sum(quantity_column).alias(quantity_column)) \
             .withColumn(rank_column, row_number().over(window_spec))
 
-    def filter_by_rank(self, data_df, rank_column, rank):
-        """
-        Filtra un DataFrame por un rango específico.
+        second_most_selling_df = ranked_df \
+            .filter(col(rank_column) == 2) \
+            .select(*select_columns)
 
-        Args:
-            data_df (pyspark.sql.DataFrame): DataFrame de entrada.
-            rank_column (str): Nombre de la columna de rango.
-            rank (int): Valor del rango a filtrar.
+        return second_most_selling_df
+    
+    def group_stores_by_category(self, base_df, arguments):
+        join_columns = arguments.get('join_columns')
+        select_columns = arguments.get('select_columns')
+        group_by_column = arguments.get('group_by_column')
+        aggregate_column = arguments.get('aggregate_column')
+        alias_name = arguments.get('alias_name')
 
-        Retorna:
-            pyspark.sql.DataFrame: DataFrame filtrado por rango.
-        """
-        return data_df.filter(col(rank_column) == rank)
+        grouped_df = base_df \
+            .join(productsDF, join_columns) \
+            .select(*select_columns) \
+            .groupBy(group_by_column) \
+            .agg(collect_list(aggregate_column).alias(alias_name))
 
-    def group_values(self, data_df, group_by_columns, aggregate_column, aggregate_function, alias_name):
-        """
-        Agrupa los valores en un DataFrame y aplica una función de agregación.
-
-        Args:
-            data_df (pyspark.sql.DataFrame): DataFrame de entrada.
-            group_by_columns (list): Lista de columnas por las cuales agrupar.
-            aggregate_column (str): Nombre de la columna a agregar.
-            aggregate_function (pyspark.sql.functions): Función de agregación a aplicar.
-            alias_name (str): Nombre del alias para el resultado.
-
-        Retorna:
-            pyspark.sql.DataFrame: DataFrame con los valores agrupados y agregados.
-        """
-        return data_df.groupBy(group_by_columns).agg(aggregate_function(aggregate_column).alias(alias_name))
-
-    def union_dataframes(self, df1, df2):
-        """
-        Une dos DataFrames en uno.
-
-        Args:
-            df1 (pyspark.sql.DataFrame): Primer DataFrame.
-            df2 (pyspark.sql.DataFrame): Segundo DataFrame.
-
-        Retorna:
-            pyspark.sql.DataFrame: DataFrame resultante de la unión de los dos DataFrames.
-        """
-        return df1.union(df2)
-
-    def process_data(self, data_paths, formats, options=None, output_path=None, result_output_path=None):
-        """
-        Procesa los datos utilizando las operaciones definidas en la clase.
-
-        Args:
-            data_paths (list): Lista de rutas de los archivos de datos.
-            formats (list): Lista de formatos de los archivos de datos.
-            options (list, optional): Lista de opciones adicionales para la lectura de datos. Default es None.
-
-        Retorna:
-            tuple: Tupla de DataFrames resultantes después del procesamiento.
-        """
-        dfs = []
-        for path, format in zip(data_paths, formats):
-            df = self.read_data(format, path, options)
-            dfs.append(df)
-
-        joined_df = self.join_tables(dfs[1], dfs[0], ["product_id"], ["ticket_id", "product_name", "country"])
-        total_quantity_df = self.calculate_total_quantity(dfs[1], "product_id", "quantity")
-        distinct_stores_df = self.count_distinct_values(dfs[1].join(dfs[0], ["product_id", "store_id"]),
-                                                        "product_id", "store_id", "num_stores")
-        ranked_stores_df = self.calculate_ranked_values(dfs[1].join(dfs[0], ["store_id"]),
-                                                        ["product_id", "store_id"], "quantity", "rank")
-        second_most_selling_df = self.filter_by_rank(ranked_stores_df, "rank", 2)
-        grouped_stores_df = self.group_values(second_most_selling_df.join(dfs[0], ["product_id"]),
-                                              "category_name", "store_id", collect_list, "stores")
-        unioned_stores_df = self.union_dataframes(dfs[0], dfs[2])
-
-        # Guardar los archivos iniciales en formato Parquet
-        if output_path:
-            # Guardar los dataframe como Delta Tables externos (.parquet)
-            dfs[0].write.format("parquet").save(output_path + "/products_parquet")
-            dfs[1].write.format("parquet").save(output_path + "/ticket_line_parquet")
-            dfs[2].write.format("parquet").save(output_path + "/stores_parquet")
-
-            # Convertir los archivos Parquet en tablas Delta externas
-            spark.sql(f"CREATE TABLE IF NOT EXISTS products_delta USING DELTA LOCATION '{output_path}/products_parquet'")
-            spark.sql(f"CREATE TABLE IF NOT EXISTS ticket_line_delta USING DELTA LOCATION '{output_path}/ticket_line_parquet'")
-            spark.sql(f"CREATE TABLE IF NOT EXISTS stores_delta USING DELTA LOCATION '{output_path}/stores_parquet'")
-        
-        if result_output_path:
-            # Guardar los resultados de los Dataframes del Challenge
-            joined_df.write.option("header", True).csv(result_output_path + "/joined_df.csv", header=True)
-            total_quantity_df.write.option("header", True).csv(result_output_path + "/total_quantity_df.csv", header=True)
-            distinct_stores_df.write.option("header", True).csv(result_output_path + "/distinct_stores_df.csv", header=True)
-            grouped_stores_df.write.option("header", True).csv(result_output_path + "/grouped_stores_df.csv", header=True)
-            unioned_stores_df.write.option("header", True).csv(result_output_path + "/unioned_stores_df.csv", header=True)
-
-        return joined_df, total_quantity_df, distinct_stores_df, grouped_stores_df, unioned_stores_df
+        return grouped_df
--- src/main.py
+++ src/main.py
@@ -15,43 +15,99 @@ def main():
     access_key = secret_data["access_key"]
     secret_access_key = secret_data["secret_key"]
 
-    # Asegurarse de tener los archivos JAR necesarios en el classpath de Spark
-    #os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.hadoop:hadoop-aws:2.10.2 org.apache.hadoop:hadoop-client:2.10.2 pyspark-shell'
-
     # Creando la configuración de Spark
     conf = SparkConf().setAppName("scrm-challenge")
     conf.set("spark.hadoop.fs.s3a.access.key", access_key)
     conf.set("spark.hadoop.fs.s3a.secret.key", secret_access_key)
     conf.set("spark.hadoop.fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    #conf.set("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.10.2,org.apache.hadoop:hadoop-client:2.10.2")
-    #conf.set("spark.jars.excludes", "com.google.guava:guava")
     
     # Creando el contexto de Spark
     sc = SparkContext(conf=conf)
     
     # Creando la sesión de Spark
     spark = SparkSession(sc).builder.appName("scrm-challenge-app").getOrCreate()
-    
-    #hadoopConf = sc._jsc.hadoopConfiguration()
-    #hadoopConf.set("fs.s3a.access.key", access_key)
-    #hadoopConf.set("fs.s3a.secret.key", secret_access_key)
-    #hadoopConf.set("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    #hadoopConf.set("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
-    
+
     data_processor = DataProcessor(spark)
     raw_path= "s3://scrm-challenge-raw/scrm/raw/data"
     data_paths = [raw_path + "/products.json",
                   raw_path + "/ticket_line.csv",
-                  raw_path + "/stores.csv",
-                  raw_path + "/stores_v2.csv"]
-    formats = ["json", "csv", "csv", "csv"]
-    options = [{"header": "true"}, {"header": "true"}, {"header": "true"}, {"header": "true"}]
-    output_path = "s3://scrm-bucket-defined/scrm/defined/data"
+                  raw_path + "/stores.csv"]
+                  
+    formats = ["json", "csv", "csv"]
+    options = [{"header": "true"}, {"header": "true"}, {"header": "true"}]
+    
     result_output_path = "s3://scrm-bucket-resultados-challenge/scrm/results/data"
 
-    joined_df, total_quantity_df, distinct_stores_df, grouped_stores_df, unioned_stores_df = \
-        data_processor.process_data(data_paths, formats, options, output_path, result_output_path)
+    product_df, ticket_line_df, stores_df = data_processor.read_data(formats, options, data_paths)
+    """
+    # Ejercicio 1:
+    
+    # Definir las columnas para unir, agrupar y contar valores distintos, y el nombre de la columna de resultado
+    join_columns = ["product_id"]
+    group_by_columns = ["product_id"]
+    distinct_count_column = "store_id"
+    result_column = "num_stores"
 
+    # Realizar las operaciones de forma secuencial
+    distinct_stores_df = data_processor.distinct_stores(base_df, join_columns, group_by_columns, distinct_count_column, result_column)
+
+    # Exportar el DataFrame resultante a la capa Defined del bucket de S3
+    distinct_stores_df.write.option("header", True).csv(result_output_path + "/1_distinct_stores_df.csv", header=True)
+    
+    # Ejercicio 2:
+    
+    # Definir los argumentos para la función calculate_second_most_selling
+    arguments = {
+        'join_columns': ["store_id"],
+        'group_by_columns': ["product_id", "store_id"],
+        'quantity_column': "quantity",
+        'rank_column': "rank",
+        'select_columns': ["product_id", "store_id", "quantity"]
+    }
+    
+    # Realizar las operaciones de forma secuencial
+    second_most_selling_df = data_processor.calculate_second_most_selling(base_df, arguments)
+    
+    # Exportar el DataFrame resultante a la capa Defined del bucket de S3
+    second_most_selling_df.write.option("header", True).csv(result_output_path + "/2_second_most_selling_df.csv", header=True)
+    
+    # Ejercicio 3:
+    
+    # Definir los argumentos para la función group_stores_by_category
+    arguments = {
+        'join_columns': secondMostSellingDF["product_id"] == productsDF["product_id"],
+        'select_columns': [productsDF["categories.category_name"].alias("category_name"), secondMostSellingDF["store_id"]],
+        'group_by_column': "category_name",
+        'aggregate_column': "store_id",
+        'alias_name': "stores"
+    }
+
+    # Realizar las operaciones de forma secuencial
+    grouped_stores_df = data_processor.group_stores_by_category(secondMostSellingDF, arguments)
+
+    # Exportar el DataFrame resultante a la capa Defined del bucket de S3
+    grouped_stores_df.write.option("header", True).csv(result_output_path + "/3_grouped_stores_df.csv", header=True)
+    
+    # Ejercicio 4:
+    
+    # Leer el archivo CSV "stores_v2.csv"
+    stores_v2_path = raw_path + "/stores_v2.csv"
+    
+    # Definir las columnas a seleccionar y las columnas para unir los DataFrames
+    
+    select_columns = [
+        col("store_id").substr(3, 2).alias("store_id"),
+        col("store_id").substr(1, 2).alias("country"),
+        "version"
+    ]
+    
+    # Definir las columnas para hacer la unión. En caso no tenga, dejar en blanco
+    join_columns = []
+    
+    updated_stores_df = data_processor.update_dataframe(stores_df, stores_v2_df, select_columns, join_columns)
+    
+    # Exportar el DataFrame resultante a la capa Defined del bucket de S3
+    unioned_stores_df.write.option("header", True).csv(result_output_path + "/4_unioned_stores_df.csv", header=True)"""
 
 if __name__ == "__main__":
     main()
